68786a7733728b1a4a7b64eae80f5fd3ea468fa4
Milos <milosmilkic445@gmail.com>
2023-10-15 20:23:15 +0200

    AddSearchTest

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
@@ -5,14 +6,28 @@ import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends BasePage{
 
-    //    accept cookies button
+    // accept cookies button
     @FindBy(xpath = "//button[normalize-space()='Prihvatam']")
     WebElement cookiesButton;
-
     @FindBy(linkText = "Otvori nalog")
     WebElement openAccount;
     @FindBy(linkText = "Prijavi se")
     WebElement login;
+
+    @FindBy(id = "search-input-header")
+    WebElement input;
+    @FindBy(css = ".reset .fa-close")
+    WebElement resetButton;
+    @FindBy(id = "search-submit-header")
+    WebElement searchButton;
+    @FindBy(css = ".sharkskin-breadcrumb .active")
+    WebElement info;
+    @FindBy(xpath = "//*[@id=\"products-list\"]/div[1]/div/div[2]/h4/a")
+    WebElement lenovoProduct;
+    @FindBy(xpath = "//*[@id=\"products-list\"]/div[3]/div/div[2]/h4/a")
+    WebElement samsungProduct;
+    @FindBy(css = ".product-name h1")
+    WebElement productInfo;
 
     public HomePage(ChromeDriver driver)
     {
@@ -21,9 +36,16 @@ public class HomePage extends BasePage{
     }
 
     public void acceptCookies() {cookiesButton.click();}
-
     public void clickOpenAccount() {openAccount.click();}
     public void clickLogin() {login.click();}
-
+    public void inputField(String name){input.sendKeys(name);}
+    public void keyEnter(){input.sendKeys(Keys.ENTER);}
+    public String inputInfo(){return input.getText();}
+    public void removeInput(){resetButton.click();}
+    public void clickSearch(){searchButton.click();}
+    public String searchResults(){return info.getText();}
+    public void clickLenovo(){lenovoProduct.click();}
+    public String productName(){return productInfo.getText();}
+    public void clickSamsung(){ samsungProduct.click();}
 
 }
